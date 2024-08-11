635f395d76a5087869a10a14163723eb23b6054d
yashr1919 <157221316+yashr1919@users.noreply.github.com>
2024-08-11 18:52:12 +0000

    dates-in-js

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,29 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 0, 23);//yr,mnth,date
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date("2024-01-14")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
