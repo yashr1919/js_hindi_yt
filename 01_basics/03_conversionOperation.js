6c6d74f80945e39efc0ca8f6bcfc3cda221db575
yashr1919 <157221316+yashr1919@users.noreply.github.com>
2024-08-11 12:06:55 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33ab"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 //JS will convert the string type to number
 //here it is not actual string so it comes NaN
 //"33" => 33
@@ -15,9 +15,40 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //"" => false
 //"yash" => true
 
+//            Operations          //
+
+let value =3
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2/3);
+
+let str1= "hello"
+let str2= " yash"
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++;
+//++gameCounter;
+console.log(gameCounter);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//always compare same datatypes
+
+// === //this is strict check
+
+//console.log("2" === 2);
+
