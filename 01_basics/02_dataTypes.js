44728dc2768137c975938dc0bde18e4b74752e97
yashr1919 <157221316+yashr1919@users.noreply.github.com>
2024-08-11 11:33:53 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newer version
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3+3);
+console.log("yash");
+//code readability should be high
+
+
+let name="yash"
+let age= 18
+let isLoggedin = false
+let state=null
+
+//number => 2 to power of 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof "yash")
