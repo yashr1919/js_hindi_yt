684cb4d4b6737248902a82dd72d68d32d79fe4f9
yashr1919 <157221316+yashr1919@users.noreply.github.com>
2024-08-16 16:03:47 +0000

    arrays-1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman", "ironman"]
+const myArr2 = new Array(1, 2, 3, 4)
+
+// console.log(myArr[1]);
+// console.log(myArr2);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(19));
+// console.log(myArr.indexOf(3));
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+//      Slice, Spice
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log("B ", myArr);
